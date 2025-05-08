d574e8157d446bb53bd7ab0ffa67bafc90c2ae1d
yatharthchavda <yatharth.9199@gmail.com>
2025-05-08 20:09:43 +0530

    added cors origin

--- server/index.js
+++ server/index.js
@@ -23,6 +23,12 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
+
+app.use(cors({
+  origin: ["sociopedia.vercel.app"],
+  methods: ["GET", "POST"],
+  credentials: true
+}))
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
