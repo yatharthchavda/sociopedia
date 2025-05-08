118b67d57e4da98a0a00090217d3dc0843cf98b5
yatharthchavda <yatharth.9199@gmail.com>
2025-05-08 21:32:30 +0530

    render dep

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,12 +7,17 @@ import { useMemo } from "react";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { themeSettings } from "theme";
+import axios from "axios";
 
 function App() {
 
     const mode = useSelector((state) => state.mode);
     const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
     const isAuth = Boolean(useSelector((state) => state.token));
+
+    const API = axios.create({
+        baseURL: process.env.REACT_APP_API_URL,
+      });
 
     return <div className="app">
         <BrowserRouter>
