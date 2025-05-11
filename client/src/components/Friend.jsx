797ea957fc6a96d80405ef42cf3e6c908ce97a59
yatharthchavda <yatharth.9199@gmail.com>
2025-05-12 01:04:34 +0530

    users route change for deployment

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -23,7 +23,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
 
     const patchFriend = async () => {
         const response = await fetch(
-            `${process.env.REACT_APP_API_URL}/users/${_id}/${friendId}`,
+            `${process.env.REACT_APP_API_URL}/users/${_id}/friends/${friendId}`,
             {
                 method: "PATCH",
                 headers: {
